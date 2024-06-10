9b9a886590bd9d8bd0269253c6933802b227d397
9-sho-5 <hosho.kusu@icloud.com>
2024-06-10 13:44:32 +0900

    init

---
+++ pencil-kit/pencil-kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pencil-kit
+//
+//  Created by ほしょ on 2024/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pencil-kit/pencil-kitUITests/pencil_kitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pencil_kitUITestsLaunchTests.swift
+//  pencil-kitUITests
+//
+//  Created by ほしょ on 2024/06/10.
+//
+
+import XCTest
+
+final class pencil_kitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
