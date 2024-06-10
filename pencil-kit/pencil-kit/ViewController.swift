1108ba047d8c65268a52a4b57808a6cf7a316f7a
9-sho-5 <hosho.kusu@icloud.com>
2024-06-10 14:29:17 +0900

    add: basic program

--- pencil-kit/pencil-kit/ViewController.swift
+++ pencil-kit/pencil-kit/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import PencilKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var pencilKitContainerView: UIView!
+    
+    private var canvasView = PKCanvasView()
+    private let pkToolPicker = PKToolPicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCanvasView()
     }
-
-
+    
+    private func setupCanvasView() {
+        configureCanvasView()
+        configureToolPicker()
+        addCanvasViewToContainer()
+    }
+    
+    private func configureCanvasView() {
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 30)
+        if #available(iOS 14.0, *) {
+            canvasView.drawingPolicy = .anyInput
+        }
+        canvasView.frame = pencilKitContainerView.bounds
+        canvasView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    private func configureToolPicker() {
+        pkToolPicker.setVisible(true, forFirstResponder: canvasView)
+        pkToolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+    
+    private func addCanvasViewToContainer() {
+        pencilKitContainerView.addSubview(canvasView)
+    }
 }
 
